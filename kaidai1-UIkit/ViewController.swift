e9a53e663cf34dab97c02d82f0b8fc9e463201cd
Aoitoridayo <s2071150@s.konan-u.ac.jp>
2023-02-01 13:23:53 +0900

    Initial Commit

---
+++ kaidai1-UIkit/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  kaidai1-UIkit
+//
+//  Created by 渡邊魁優 on 2023/02/01.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
