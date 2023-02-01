dd68cdefbdc2f38ddc7c1511f1e60a2d87913f4a
Aoitoridayo <s2071150@s.konan-u.ac.jp>
2023-02-01 13:48:00 +0900

    commit

--- kaidai1-UIkit/ViewController.swift
+++ kaidai1-UIkit/ViewController.swift
@@ -1,19 +1,18 @@
-//
-//  ViewController.swift
-//  kaidai1-UIkit
-//
-//  Created by 渡邊魁優 on 2023/02/01.
-//
 
 import UIKit
 
 class ViewController: UIViewController {
+    //privateをつける
+    @IBOutlet weak private var numberLabel: UILabel!
+    @IBOutlet weak private var number1: UITextField!
+    @IBOutlet weak private var number2: UITextField!
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
+    @IBAction func calclation(_ sender: Any) {
+        var numberInput1 = Int(number1.text ?? "") ?? 0
+        var numberInput2 = Int(number2.text ?? "") ?? 0
+        var result = String(numberInput1 + numberInput2)
+        numberLabel.text = result
     }
-
-
+    
 }
 
